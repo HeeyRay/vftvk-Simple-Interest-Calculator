5cdfc1313d9327b85ce30d8abbce72f56f83f33a
Raymond Lau <36816984+HeeyRay@users.noreply.github.com>
2021-11-23 16:05:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function compute()
 {
-     
+     var principal = document.getElementById("principal").value;
+     var rate = document.getElementById("rate").value;
+     var years = document.getElementById("years").value;
+     var interest = principal * years * rate /100;
+     var year = new Date().getFullYear()+parseInt(years);
+     var amount = principal + interest; 
     if(principal.value<=0){
           alert("Enter a positive number");
           fname.focus();
     }
     else {
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-var amount = principal + interest;
+
          document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 
 }
